dbd2cbc44ed161e1d4838517fac27cfa844dfb94
Christopher Hicks <visrule@protonmail.com>
2021-01-27 19:27:43 -0800

    Initial Commit

---
+++ Connections/ConnectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectionsApp.swift
+//  Connections
+//
+//  Created by Christopher Hicks on 1/27/21.
+//
+
+import SwiftUI
+
+@main
+struct ConnectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
