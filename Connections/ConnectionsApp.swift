438db04e6f03f21b8a7e1e5e9c7dade3c26b5ff8
Christopher Hicks <visrule@protonmail.com>
2021-01-28 17:23:59 -0800

    Circles & Faces

--- Connections/ConnectionsApp.swift
+++ Connections/ConnectionsApp.swift
@@ -11,7 +11,43 @@ import SwiftUI
 struct ConnectionsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            
+            ZStack {
+                
+                Divider().rotationEffect(.degrees(30))
+                Divider().rotationEffect(.degrees(90))
+                Divider().rotationEffect(.degrees(-30))
+                
+                ConnectionView(imageName: "omboi")
+                
+                HStack {
+                    ConnectionView(imageName: "faceGuy")
+                        .rotationEffect(.degrees(-30))
+                    Spacer()
+                    ConnectionView(imageName: "hatWoman")
+                        .rotationEffect(.degrees(-30))
+                    
+                }.rotationEffect(.degrees(30))
+                
+                HStack {
+                    ConnectionView(imageName: "horseMan")
+                        .rotationEffect(.degrees(-90))
+                    Spacer()
+                    ConnectionView(imageName: "sexyGirl")
+                        .rotationEffect(.degrees(-90))
+                }.rotationEffect(.degrees(90))
+                
+                HStack {
+                    ConnectionView(imageName: "prettyLady")
+                        .rotationEffect(.degrees(30))
+                    Spacer()
+                    ConnectionView(imageName: "nativeGuy")
+                        .rotationEffect(.degrees(30))
+                }.rotationEffect(.degrees(-30))
+                
+            }.frame(width: 300, height: 300)
         }
     }
 }
+
+
